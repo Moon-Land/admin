45be196f7fd265e8b7038906f18a2a80de63f338
Gavin Gong <kefengong@gmail.com>
2018-09-26 10:47:51 +0800

    top-nav

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <div class="main">
     <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
       <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
         <Submenu name="1">
@@ -12,7 +12,20 @@
       </Menu>
     </Sider>
     <Layout :style="{marginLeft: '200px'}">
-      <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
+      <Header class="header" :style="{background: '#fff'}">
+        <div class="left">
+          <Icon type="md-code-working" size="28" class="toggle-icon"/>
+        </div>
+        <div class="right">
+          <Badge dot>
+            <Icon type="ios-notifications-outline" size="26"></Icon>
+          </Badge>
+          <div class="avatar">
+            <Avatar src="https://i.loli.net/2017/08/21/599a521472424.jpg" />
+            <span>Zen</span>
+          </div>
+        </div>
+      </Header>
       <Content :style="{padding: '0 16px 16px'}">
         <!-- <Breadcrumb :style="{margin: '16px 0'}">
           <BreadcrumbItem>Home</BreadcrumbItem>
@@ -30,7 +43,7 @@
 
   }
 </script>
-<style scoped>
+<style scoped lang="scss">
 .layout {
   border: 1px solid #d7dde4;
   background: #f5f7f9;
@@ -42,5 +55,30 @@
 .layout-header-bar {
   background: #fff;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.right {
+  display: inline-flex;
+  align-items: center;
+  /deep/ .ivu-badge {
+    line-height: 1.5;
+    margin-right: 20px;
+  }
+}
+.avatar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  width: 60px;
+}
+.main {
+  /deep/ .ivu-layout-header {
+    // padding: 0 50px;
+  }
 }
 </style>
